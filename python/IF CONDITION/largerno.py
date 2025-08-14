cd1ea755a651fd5eddb7071eccad01ee2361c74f
kumaraditya62 <kumaraditya10vaishali@gmail.com>
2025-08-14 17:02:52 +0530

    program to find larger of two numbers

---
+++ python/IF CONDITION/largerno.py
@@ -0,0 +1,8 @@
+#program to find larger of two numbers
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+if a > b:
+    larger = a
+else:
+    larger = b
+print("Larger number is:", larger)
